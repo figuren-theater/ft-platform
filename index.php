ec3f90dfdddc66040145116ffdc98ab5b79a7bb2
carstenbach <mail@carsten-bach.de>
2023-04-23 16:57:39 +0200

    Add folder-structure esp. for themes

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * WordPress bootstrapper
+ *
+ * @package Figuren_Theater
+ */
+
+/**
+ * What is the constant WP_USE_THEMES for?
+ *
+ * @see https://wordpress.stackexchange.com/a/12923
+ */
+define( 'WP_USE_THEMES', true );
+
+// WordPress view bootstrapper
+require './wp/wp-blog-header.php';
